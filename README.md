ad51a290e56ce0398feaa8cfe7ff71b782602822
takokun778 <takokun.778@gmail.com>
2022-10-30 00:21:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fs-tf-cockroach
+
+```bash
+terraform init
+```
+
+```bash
+export COCKROACH_API_KEY="xxxx" terraform apply
+```
+
+```bash
+export COCKROACH_API_KEY="xxxx" terraform destroy
+```
---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    cockroach = {
+      source  = "cockroachdb/cockroach"
+      version = "0.2.0"
+    }
+  }
+}
+
+variable "cluster_name" {
+  type     = string
+  nullable = false
+}
+
+variable "sql_user_name" {
+  type     = string
+  nullable = false
+  default  = "maxroach"
+}
+
+variable "sql_user_password" {
+  type      = string
+  nullable  = false
+  sensitive = true
+}
+
+variable "serverless_spend_limit" {
+  type     = number
+  nullable = false
+  default  = 0
+}
+
+variable "cloud_provider" {
+  type     = string
+  nullable = false
+  default  = "GCP"
+}
+
+variable "cloud_provider_regions" {
+  type     = list(string)
+  nullable = false
+  default  = ["asia-southeast1"]
+}
+
+provider "cockroach" {
+  # export COCKROACH_API_KEY with the cockroach cloud API Key
+}
+
+resource "cockroach_cluster" "cockroach" {
+  name           = var.cluster_name
+  cloud_provider = var.cloud_provider
+  serverless = {
+    spend_limit = var.serverless_spend_limit
+  }
+  regions = [for r in var.cloud_provider_regions : { name = r }]
+}
+
+resource "cockroach_sql_user" "cockroach" {
+  id = cockroach_cluster.cockroach.id
+  name       = var.sql_user_name
+  password   = var.sql_user_password
+}
